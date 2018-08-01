ad2f92b4be8a1d8465aca17cb8fe388e86f79622
Jørgen Elgaard Larsen <jel@elgaard.net>
2018-08-01 22:09:44 +0200

    Only import if file is new
    Also sql test script

--- getDK.sh
+++ getDK.sh
@@ -10,20 +10,28 @@ echo "Getting file"
 wget -nv -P files -N "http://download.geofabrik.de/europe/denmark-latest.osm.pbf"
 
 # Import into database
-PGPORT=5435 osm2pgsql -s -G -K -j -c -l --prefix='osm' -S osmimport.style -U ${POSTGIS_DBUSER} -d osm files/denmark-latest.osm.pbf
-
+if test $(find files/denmark-latest.osm.pbf -mmin -240)
+then
+  PGPORT=5435 osm2pgsql -s -G -K -j -c -l --prefix='osm' -S osmimport.style -U ${POSTGIS_DBUSER} -d osm files/denmark-latest.osm.pbf
+else
+  echo "No news from OSM"
+fi
 
 # Get official Danish placenames from https://download.kortforsyningen.dk/content/stednavne and unzip it.
 wget -nv -P files -N ftp://${KORTFORSYNINGEN_USER}:${KORTFORSYNINGEN_PW}@ftp.kortforsyningen.dk/stednavne/stednavne/GML/DK_GML_UTM32-EUREF89.zip
-unzip -j -o files/DK_GML_UTM32-EUREF89.zip KORT10/KORT10.gml -d files
+if test $(find files/DK_GML_UTML32-EUREF89.zip -mmin -240)
+then
+   unzip -j -o files/DK_GML_UTM32-EUREF89.zip KORT10/KORT10.gml -d files
 
-# Fix missing dimension and SRS definitions and surplus spaces in the GML file:
-perl -p -e 's/<gml:posList>/<gml:posList srsName="EPSG:25832" srsDimension="3">/g; s/\s+(<\/kort)/$1/gs' files/KORT10.gml > files/stednavne.gml
+   # Fix missing dimension and SRS definitions and surplus spaces in the GML file:
+   perl -p -e 's/<gml:posList>/<gml:posList srsName="EPSG:25832" srsDimension="3">/g; s/\s+(<\/kort)/$1/gs' files/KORT10.gml > files/stednavne.gml
 
-
-# Import placenames to PostGIS
-echo "Importing stednavne"
-PGPORT=5435 ogr2ogr -f PostgreSQL -dim XY -nln stednavne -skipfailures -preserve_fid -overwrite -t_srs WGS84 -lco GEOMETRY_NAME=way PG:"dbname=osm user=${POSTGIS_USER}" files/stednavne.gml
+   # Import placenames to PostGIS
+   echo "Importing stednavne"
+   PGPORT=5435 ogr2ogr -f PostgreSQL -dim XY -nln stednavne -skipfailures -preserve_fid -overwrite -t_srs WGS84 -lco GEOMETRY_NAME=way PG:"dbname=osm user=${POSTGIS_USER}" files/stednavne.gml
+else
+   echo "No news from Kortforsyningen"
+fi
 
 echo "Creating indeces"
 # Create indexes
---
+++ test_sql.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Script for exporting GeoJSON layers for missing features in Denmark
+
+for sqlfile in $(ls layer_sql/*.sql); do
+    sqlfile=${sqlfile#*/}
+    layer=${sqlfile%.sql}
+    echo "Testing $layer"
+    cat layer_sql/${sqlfile} sql/limit.sql | psql osm
+done
+
+
+echo "Done"
+
