cc5324360fb1baf84b8bc8f9db70ef5f810b9827
Jørgen Elgaard Larsen <jel@elgaard.net>
2018-08-04 11:56:42 +0200

    Check for stations in missin_trains

--- layer_sql/missing_trains.sql
+++ layer_sql/missing_trains.sql
@@ -6,12 +6,23 @@ where featuretype in (
                      )
 and not exists (select 1
                 from osm_polygon p
-		where p.tags -> 'railway' IN ('narrow_gauge', 'preserved', 'rail', 'subway', 'light_rail', 'disused', 'abandoned')
+		where (   p.tags -> 'railway' IN ('narrow_gauge', 'preserved', 'rail', 'subway', 'light_rail', 'disused', 'abandoned', 'station', 'halt')
+                       OR p.tags -> 'building' IN ('station')
+                       OR p.tags -> 'public_transport' IN ('station')
+                      )
 		 AND (p.name = s.navn OR p.alt_name = s.navn)
 		 AND ST_Distance(p.geog, s.geog) < 20 )
 
 and not exists (select 1
                 from osm_line p
 		where p.tags -> 'railway' IN ('narrow_gauge', 'preserved', 'rail', 'subway', 'light_rail', 'disused', 'abandoned')
+		 AND (p.name = s.navn OR p.alt_name = s.navn)
+		 AND ST_Distance(p.geog, s.geog) < 20 )
+
+and not exists (select 1
+                from osm_point p
+		where (   p.tags -> 'railway' IN ('halt', 'station')
+                       OR p.tags -> 'public_transport' IN ('station')
+                      )
 		 AND (p.name = s.navn OR p.alt_name = s.navn)
 		 AND ST_Distance(p.geog, s.geog) < 20 )
