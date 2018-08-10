22f964d3379cc572b216f118cc6c8d02b0890837
Jørgen Elgaard Larsen <jel@elgaard.net>
2018-08-10 14:54:02 +0200

    Do not flag bridges without addresses

--- layer_sql/lonely_roads.sql
+++ layer_sql/lonely_roads.sql
@@ -4,6 +4,7 @@ where name is not null
  and name <> ''
  and defined(tags, 'highway')
  and tags -> 'highway' not in ('motorway', 'motorway_link', 'trunk_link', 'primary_link', 'secondary_link', 'footway', 'path', 'cycleway', 'proposed', 'construction', 'track', 'primary')
+ and not defined(tags, 'bridge')
  and not exists (select 1
                  from osm_point a
   		 where (r.name = a.tags -> 'addr:street') 
