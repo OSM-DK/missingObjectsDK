f4817e0c98839f82d022c5cd6822f02ec71ba744
Jørgen Elgaard Larsen <jel@elgaard.net>
2018-08-10 14:58:10 +0200

    Added check for embankment for "dæmning"

--- layer_sql/missing_water.sql
+++ layer_sql/missing_water.sql
@@ -6,6 +6,8 @@ and not exists (select 1
 		where (   p.tags -> 'waterway' in ('river', 'stream', 'riverbank', 'canal', 'waterfall', 'dam', 'lock_gate')
                        OR p.tags -> 'lock' = 'yes'
                        OR p.tags -> 'water' = 'lock'
+                       OR defined(p.tags, 'embankment')
+                       OR p.tags -> 'man_made' IN ('embankment')
                       )
 		 AND (p.name = s.navn OR p.alt_name = s.navn OR p.tags -> 'lock_name' = s.navn)
 		 AND ST_Distance(p.geog, s.geog) < 50 )
