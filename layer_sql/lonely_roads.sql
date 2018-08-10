4d87566ef727894bc440cbbe9dc3c41f54de2094
Jørgen Elgaard Larsen <jel@elgaard.net>
2018-08-10 14:35:12 +0200

    Do not flag highway=construction|proposed|track|primary without addresses

--- layer_sql/lonely_roads.sql
+++ layer_sql/lonely_roads.sql
@@ -3,7 +3,7 @@ from osm_line r
 where name is not null
  and name <> ''
  and defined(tags, 'highway')
- and tags -> 'highway' not in ('motorway', 'motorway_link', 'trunk_link', 'primary_link', 'secondary_link', 'footway', 'path', 'cycleway')
+ and tags -> 'highway' not in ('motorway', 'motorway_link', 'trunk_link', 'primary_link', 'secondary_link', 'footway', 'path', 'cycleway', 'proposed', 'construction', 'track', 'primary')
  and not exists (select 1
                  from osm_point a
   		 where (r.name = a.tags -> 'addr:street') 
