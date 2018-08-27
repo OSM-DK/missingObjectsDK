bbc8cff0c5b59c51f74f0a4858a16a5084692ab0
Jørgen Elgaard Larsen <jel@elgaard.net>
2018-08-27 19:30:31 +0200

    Validate GeoJSON files before copying

--- copy_layers.sh
+++ copy_layers.sh
@@ -3,14 +3,19 @@
 # Script for copying working layers to home page
 source config.sh
 
-echo "Staring copy_layers $(date)" >> $LOGFILE
+echo "Starting copy_layers $(date)" >> $LOGFILE
 for layerfile in $(ls layers/*.geojson); do
-	layerfile=${layerfile#*/}
+    layerpath="layers/${layerfile#*/}"
 
-	# TODO: Validate geojson file
-	cp "layers/$layerfile" public/layers >> $LOGFILE
+    ./validate_layer.js $layerpath
+    validation_result=$?
+    if [ $validation_result -eq 0 ]
+    then
+       cp "$layerpath" public/layers >> $LOGFILE
+       echo "Copied ${layerpath}" >> $LOGFILE
+    else
+       echo "Skipped invalid file ${layerpath}" >> $LOGFILE
+    fi
 done
 
-
 echo "Done - $(date)" >> $LOGFILE
-
---
+++ validate_layer.js
@@ -0,0 +1,73 @@
+#!/usr/bin/env node
+
+// Script to validate a GeoJSON file
+
+
+'use strict';
+
+const fs = require('fs');
+const GJV = require('geojson-validation');
+
+
+function usage(msg) {
+  if (msg) {
+    console.error(msg);
+  }
+  console.error("Usage: validate_layer.js filename\n  e.g: validate_layer.js my_layer.geojson");
+}
+
+
+if (process.argv.length != 3) {
+  usage('Wrong number of arguments');
+  process.exit(3);
+}
+
+
+const fileName = process.argv[2];
+
+if (! fs.existsSync(fileName)) {
+  usage(`File '${fileName}' does not exist.`);
+  process.exit(4);
+}
+
+const stats = fs.statSync(fileName);
+if (!stats || stats.size < 1) {
+  console.error(`File ${fileName} is only ${stats.size} byte.`);
+  process.exit(30);
+}
+
+var content;
+try {
+  content = fs.readFileSync(fileName);
+} catch ( e ) {
+  console.error(`Could not read GeoJSON file ${fileName}`, e);
+  process.exit(6);
+}
+
+var data;
+try {
+  data = JSON.parse(content);
+} catch(e) {
+  console.error(`Could not parse GeoJSON file ${fileName} as JSON`, e);
+  process.exit(70);
+}
+
+if (!data || !data.features || data.features.length < 1) {
+  console.error(`GeoJSON file ${fileName} has no features`);
+  process.exit(71);
+}
+
+
+try {
+  if (!GJV.valid(data)) {
+    console.error(`File ${fileName} is not valid GeoJSON`);
+    process.exit(50);
+  }
+} catch (e) {
+  console.error(`Could not validate GeoJSON file ${fileName}`, e);
+  process.exit(51);
+}
+
+
+// If we get here. everything is good
+process.exit(0);
