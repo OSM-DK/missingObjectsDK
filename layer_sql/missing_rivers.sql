85440bb89c60d667a36b5ed0bcbcbfc463dcfbf2
Jørgen Elgaard Larsen <jel@elgaard.net>
2022-01-02 12:13:04 +0100

    Added waterway=drain to missing_rivers

--- layer_sql/missing_rivers.sql
+++ layer_sql/missing_rivers.sql
@@ -3,7 +3,7 @@ from stednavne.vandloeb s
 where
    not exists (select 1
                 from osm_line p, osm_names n, stednavne_names sn
-		where (   p.tags -> 'waterway' in ('river', 'stream', 'riverbank', 'canal', 'ditch')
+		where (   p.tags -> 'waterway' in ('river', 'stream', 'riverbank', 'canal', 'ditch', 'drain')
                        OR defined(p.tags, 'embankment')
                        OR p.tags -> 'man_made' IN ('embankment')
                       )
