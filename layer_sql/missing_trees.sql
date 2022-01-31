7fa90f9579314d908e4db10dcea01b44fa839684
Jørgen Elgaard Larsen <jel@elgaard.net>
2022-01-31 11:17:19 +0100

    Increased tolerance for trees

--- layer_sql/missing_trees.sql
+++ layer_sql/missing_trees.sql
@@ -18,7 +18,8 @@ and not exists (select 1
                  AND n.osm_id = p.osm_id
 		 AND n.name = sn.name
                  AND sn.gml_id = s.gml_id
-		 AND ST_Distance(p.geog, s.geog) < 1 )
+		 AND ST_DWithin(p.geog, s.geog, 30)
+		)
 
 
 and not exists (select 1
@@ -31,7 +32,8 @@ and not exists (select 1
                  AND n.osm_id = p.osm_id
 		 AND n.name = sn.name
                  AND sn.gml_id = s.gml_id
-		 AND ST_Distance(p.geog, s.geog) < 1 )
+		 AND ST_DWithin(p.geog, s.geog, 30)
+		)
 
 
 and not exists (select 1
@@ -44,4 +46,5 @@ and not exists (select 1
                  AND n.osm_id = p.osm_id
 		 AND n.name = sn.name
                  AND sn.gml_id = s.gml_id
-		 AND ST_Distance(p.geog, s.geog) < 1 )
+		 AND ST_DWithin(p.geog, s.geog, 30)
+		)
