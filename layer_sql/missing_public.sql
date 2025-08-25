7b05544729a0487b09b1d92d49e8c0e4bf3a146a
Kasper Kirkegaard <KasperLK@users.noreply.github.com>
2025-08-25 15:13:24 +0200

    Update missing_public.sql
    Add amenity=clinic and healthcare=clinic to handle emergency clinics.

--- layer_sql/missing_public.sql
+++ layer_sql/missing_public.sql
@@ -1,8 +1,8 @@
 select way, ogc_fid, gml_id, objectid, bygningstype as featuretype, navn_1_skrivemaade as navn, 'bygning' as featureclass
 from stednavne.bygning s
 where bygningstype in (
- 		      'daginstitution',
- 		      'fængsel',
+                      'daginstitution',
+                      'fængsel',
                       'hospital',
                       'kommunekontor',
                       'regionsgård',
@@ -11,32 +11,34 @@ where bygningstype in (
                      )
 and not exists (select 1
                 from osm_polygon p, osm_names n, stednavne_names sn
-		where (   p.tags -> 'amenity' in ( 'prison',
+                where (   p.tags -> 'amenity' in ( 'prison',
                                                    'kindergarten',
                                                    'hospital',
-				   	           'social_facility',
-					           'townhall'
-					          )
-                       OR p.tags -> 'healthcare' in ( 'hospice' )
+                                                   'clinic',
+                                                   'social_facility',
+                                                   'townhall'
+                                                 )
+                       OR p.tags -> 'healthcare' in ( 'hospice', 'clinic' )
                        OR p.tags -> 'emergency' in ( 'emergency_ward_entrance' )
                       )
                  AND n.osm_id = p.osm_id
-		 AND n.name = sn.name
+                 AND n.name = sn.name
                  AND sn.gml_id = s.gml_id
-		 AND ST_Distance(p.geog, s.geog) < 20 )
+                 AND ST_Distance(p.geog, s.geog) < 20 )
 
 and not exists (select 1
                 from osm_point p, osm_names n, stednavne_names sn
-		where (   p.tags -> 'amenity' in ( 'prison',
+                where (   p.tags -> 'amenity' in ( 'prison',
                                                    'kindergarten',
                                                    'hospital',
-				   	           'social_facility',
-					           'townhall'
-					          )
-                       OR p.tags -> 'healthcare' in ( 'hospice' )	
+                                                   'clinic',
+                                                   'social_facility',
+                                                   'townhall'
+                                                 )
+                       OR p.tags -> 'healthcare' in ( 'hospice', 'clinic' )	
                        OR p.tags -> 'emergency' in ( 'emergency_ward_entrance' )
                       )
                  AND n.osm_id = p.osm_id
-		 AND n.name = sn.name
+                 AND n.name = sn.name
                  AND sn.gml_id = s.gml_id
-		 AND ST_Distance(p.geog, s.geog) < 20 )
+                 AND ST_Distance(p.geog, s.geog) < 20 )
